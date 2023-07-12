c4ca9f8dab0cc9ac65209a6bf5286aeed01a88e8
Mark Omarov <15357910+mark-omarov@users.noreply.github.com>
2023-07-13 01:40:09 +0900

    solve factorials test

--- specs/recursion/factorials.test.js
+++ specs/recursion/factorials.test.js
@@ -10,11 +10,13 @@
   factorial(3) = 6 
 */
 
-function factorial(n) {}
+function factorial(n) {
+  return n * (n === 1 ? n : factorial(n - 1));
+}
 
 // unit tests
 // do not modify the below code
-test.skip("factorials", () => {
+test("factorials", () => {
   expect(factorial(1)).toEqual(1);
   expect(factorial(2)).toEqual(2);
   expect(factorial(3)).toEqual(6);
